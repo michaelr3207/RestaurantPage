f3a8475751e456dda8b574cd3cdb211c168ca921
michaelr3207 <michael--robinson321@hotmail.com>
2024-01-14 23:02:19 +0000

    Getting styling started for initial webpage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
-alert('this is a test');
-console.log('this is a test aswell');
+import {loadHomePage} from './loadHomePage'
+import './style.css';
+import _ from "lodash";
+
+
+
+// loadHomePage();
+// setWebPageStyle();
+//
+// console.log('this is a test aswell');
+//
+//
+//
+// function setWebPageStyle() {
+//     document.body.style.background = 'linear-gradient(122deg, #171717, #2c200a)';
+//     const contentDiv = createContentDiv();
+//     const bodyDiv = document.createElement('div');
+//     bodyDiv.id = 'mainBody';
+//     document.getElementById('content').appendChild(bodyDiv);
+// }
+//
+//
+// function createContentDiv(){
+//     const contentElement = document.createElement('div');
+//     contentElement.id = 'content';
+//     document.body.appendChild(contentElement);
+//     return contentElement
+// }
+
+function component() {
+//     const contentDiv = document.getElementById('content');
+//     const headerElement = document.createElement('div');
+//     headerElement.id = 'headerDiv'; headerElement.classList.add('headerDiv');
+//     contentDiv.appendChild(headerElement);
+ }
+
+component();
+// function componentHello() {
+//     const element = document.createElement('div');
+//
+//     // Lodash, currently included via a script
+//     element.innerHTML = myName('Bruh');
+//     return element;
+// }
+
+// document.body.appendChild(component());
---
+++ src/loadHomePage.js
@@ -0,0 +1,4 @@
+function loadHomePage(){
+}
+
+export {loadHomePage};
