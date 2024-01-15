b89db9ca7c27c3ef989abd33a27a1f3f5da6357b
michaelr3207 <michael--robinson321@hotmail.com>
2024-01-15 15:44:42 +0000

    More added to UI on home page

--- src/index.js
+++ src/index.js
@@ -3,45 +3,9 @@ import {loadHomePage} from './loadHomePage'
 import _ from "lodash";
 
 
-
-// loadHomePage();
-// setWebPageStyle();
-//
-// console.log('this is a test aswell');
-//
-//
-//
-// function setWebPageStyle() {
-//     document.body.style.background = 'linear-gradient(122deg, #171717, #2c200a)';
-//     const contentDiv = createContentDiv();
-//     const bodyDiv = document.createElement('div');
-//     bodyDiv.id = 'mainBody';
-//     document.getElementById('content').appendChild(bodyDiv);
-// }
-//
-//
-// function createContentDiv(){
-//     const contentElement = document.createElement('div');
-//     contentElement.id = 'content';
-//     document.body.appendChild(contentElement);
-//     return contentElement
-// }
-
 function component() {
  loadHomePage();
-//     const contentDiv = document.getElementById('content');
-//     const headerElement = document.createElement('div');
-//     headerElement.id = 'headerDiv'; headerElement.classList.add('headerDiv');
-//     contentDiv.appendChild(headerElement);
+
  }
 
 component();
-// function componentHello() {
-//     const element = document.createElement('div');
-//
-//     // Lodash, currently included via a script
-//     element.innerHTML = myName('Bruh');
-//     return element;
-// }
-
-// document.body.appendChild(component());
--- src/loadHomePage.js
+++ src/loadHomePage.js
@@ -1,5 +1,7 @@
 import CoffeeImage from './coffeeCUp2.jpeg'
 
+
+
 function loadHomePage(){
     // const coffeeBackgroundImage = new Image();
     // coffeeBackgroundImage.src = CoffeeImage;
@@ -14,7 +16,9 @@ function createHeader() {
     headerElement.classList.add('headerDiv'); headerElement.id = 'header';
     const contentDiv = document.getElementById('contentBox');
     headerElement.appendChild(createHeaderTitle());
+    headerElement.appendChild(addButtonsToHeader());
     contentDiv.appendChild(headerElement);
+    createMainBodyDiv();
 }
 
 function createHeaderTitle(){
@@ -24,4 +28,61 @@ function createHeaderTitle(){
     return headerTitleDiv;
 }
 
+function addButtonsToHeader(){
+    const headerButtons = document.createElement('div');
+    headerButtons.classList.add('headerButtonDiv');
+    const button1 = document.createElement('button');
+    const button2 = document.createElement('button');
+    const button3 = document.createElement('button');
+    button1.innerHTML = 'Home';
+    button2.innerHTML = 'Menu';
+    button3.innerHTML = 'Contact';
+    headerButtons.appendChild(button1);
+    headerButtons.appendChild(button2);
+    headerButtons.appendChild(button3);
+    return headerButtons;
+}
+
+function createMainBodyDiv(){
+    const mainBodyElement = document.createElement('div');
+    const mainBodyMessageDiv = document.createElement('div');
+    const smallMessage = document.createElement('div');
+    const introMessage = document.createElement('div');
+    introMessage.classList.add('introMsgDiv');
+    introMessage.innerHTML = 'The Best Coffee I have Ever Had...';
+    smallMessage.innerHTML = 'Please be aware of waiting times during holidays';
+    smallMessage.classList.add('smallMsg');
+    mainBodyElement.appendChild(introMessage);
+    mainBodyElement.classList.add('mainBodyDiv');
+    mainBodyMessageDiv.classList.add('mainBodyMessageDiv');
+    mainBodyMessageDiv.innerHTML = 'Welcome to our coffee shop website please have a loook around, we have a' +
+        'wide selection on offer!';
+    mainBodyMessageDiv.appendChild(smallMessage);
+    mainBodyElement.appendChild(mainBodyMessageDiv);
+    mainBodyElement.appendChild(createButtonBoxDiv());
+    mainBodyElement.appendChild(createOpeningTimeMsg());
+    const contentDiv = document.getElementById('contentBox');
+    contentDiv.appendChild(mainBodyElement);
+}
+
+function createButtonBoxDiv(){
+    const btnBoxElement = document.createElement('div');
+    btnBoxElement.classList.add('btnBoxDiv');
+    const orderBtn = document.createElement('button');
+    const learnMoreBtn = document.createElement('button');
+    learnMoreBtn.id  = 'learnMoreBtn';
+    learnMoreBtn.innerHTML = 'Learn More';
+    orderBtn.innerHTML = 'Order Now';
+    btnBoxElement.appendChild(orderBtn);
+    btnBoxElement.appendChild(learnMoreBtn);
+    return btnBoxElement;
+}
+
+
+function createOpeningTimeMsg(){
+    const openingTimesMessageElement = document.createElement('div');
+    openingTimesMessageElement.innerHTML = 'Open 9 - 5 Mon to Friday!';
+    openingTimesMessageElement.classList.add('openingTimesDiv');
+    return openingTimesMessageElement;
+}
 export {loadHomePage};
